7109ff16bed14acc37d62e0729e078a9a0045400
Erica1912 <eri.ruiz@hotmail.com.ar>
2021-04-25 10:14:46 -0500

    agregando los porcentajes de días por mes, validación de días

--- src/components/StatusBar.jsx
+++ src/components/StatusBar.jsx
@@ -3,36 +3,47 @@ import "../asset/StatusBar.css";
 
 const StatusBar = (api) => {
   const [{ days, title, current_status }] = Object.values(api.value);
-
+  let contador = 0;
+  let porcentaje=0
+  for (let i = 0; i < days.length; i++) {
+    contador = days[i]+contador;     
+  }
+  porcentaje = Math.round(contador/30)
+  
   return (
     <div className="principal-statusbar">
-      
       <div className="info">
-      <div >{title}</div>
-        {days.map((day) => {
-          if (day <= 60) {
-            return <div className="menor"></div>;
-          }
-          if (day > 60 && day < 80) {
-            return <div className="entre"></div>;
-          }
-          if (day > 80) {
-            return <div className="mayor"></div>;
-          }
-        })}
+        <div className="titulos_status">
+          <div className="texto-info">{title}</div>
+          <div className="status">{current_status}</div>
+        </div>
+        <div className="principal-bar">
+          {days.map((day) => {
+            if (day <= 60) {
+              return <div className="menor"></div>;
+            }
+            if (day > 60 && day < 80) {
+              return <div className="entre"></div>;
+            }
+            if (day > 80) {
+              return <div className="mayor"></div>;
+            }
+          })}
+        </div>
+        <div className="principal-porcentaje">
+          <div className="porcentaje">{porcentaje} %</div>
+          <div className="dia">Today</div>
+        </div>
+      </div>
       
-      </div>  
     </div>
   );
 };
 
 /*
-<p>{title}</p>
-      <p>{current_status}</p>
-      <ul>
-        {days.map((day) => (
-          <li>{day}</li>
-        ))}
-      </ul>*/
+
+ 
+
+*/
 
 export default StatusBar;

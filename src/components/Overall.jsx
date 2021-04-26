4bd026e2a6bc1804f86bb81f884cf945a74f5e0f
Erica1912 <eri.ruiz@hotmail.com.ar>
2021-04-25 19:56:33 -0500

    finishing project

--- src/components/Overall.jsx
+++ src/components/Overall.jsx
@@ -3,20 +3,31 @@ import "../asset/Overall.css";
 
 /**
  * Overall component
- * 
- * @author Erica Ruiz <eri.ruiz@hotmail.com> 
- * 
+ *
+ * @author Erica Ruiz <eri.ruiz@hotmail.com>
+ *
  * @param {React.ComponentProps} props of the Overall
  * @returns {JSX.Element} Overall
  */
 const Overall = (props) => {
-  const {  average24h, average7d, average30d } = props.values;
-  
-  return (<div>
-      <div>{Math.round( average24h)} %</div>
-      <div>{Math.round(average7d)} %</div>
-      <div>{Math.round(average30d)} %</div>
-  </div>);
+  const { average24h, average7d, average30d } = props.values;
+
+  return (
+    <div className="overall-principal">
+      <div className="overall-text">OVERALL UPTIME</div>
+      <div className="overall-info">
+        <div className="overall-datos">
+          {Math.round(average24h)} %<p className="overall-p"> Last 24 Hours</p>
+        </div>
+        <div className="overall-datos">
+          {Math.round(average7d)} %<p className="overall-p"> Last 7 Days</p>
+        </div>
+        <div className="overall-datos">
+          {Math.round(average30d)} %<p className="overall-p"> Last 30 Days</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Overall;
